f3e2398a39d81642b79b337b43b51db86e08e715
m2bass <121668589+m2bass@users.noreply.github.com>
2024-11-18 09:45:13 -0400

    Add files via upload

--- route/views.py
+++ route/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Route
 from .forms import RouteForm
-from django.http import JsonResponse
-from django.urls import reverse
 
 # List all Routes
 def route_list(request):
@@ -15,20 +13,11 @@ def route_create(request):
     if request.method == 'POST':
         form = RouteForm(request.POST)
         if form.is_valid():
-            route = form.save()
-            # Return route details in JSON for dynamically updating the table
-            return JsonResponse({
-                'success': True,
-                'route': {
-                    'route_short_name': route.route_short_name,
-                    'route_long_name': route.route_long_name,
-                    'update_url': reverse('route_update', args=[route.id]),
-                    'delete_url': reverse('route_delete', args=[route.id]),
-                }
-            })
-        else:
-            return JsonResponse({'success': False, 'error': form.errors}, status=400)
-    return JsonResponse({'success': False}, status=405)
+            form.save()
+            return redirect('route_list')
+    else:
+        form = RouteForm()
+    return render(request, 'route/route_form.html', {'form': form})
 
 # Update an Existing Route
 def route_update(request, route_id):  # Change pk to route_id
